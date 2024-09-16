28dc36829e1414f1bc3e6121d3711861ca593705
vqlinhSW <linhvuong@smartwave247.com>
2024-09-16 13:44:30 +0700

    + final

--- Assets/Code/ToolLoadScene.cs
+++ Assets/Code/ToolLoadScene.cs
@@ -1,35 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SceneManagement;
-using UnityEditor;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEditor.SceneManagement;
+//using UnityEditor;
+//using UnityEngine;
 
-public class ToolLoadScene : MonoBehaviour
-{
-    [MenuItem("Open Scene/Load &1")]
-    public static void OpenSceneRoot()
-    {
-        OpenScene("Loading");
-    }
+//public class ToolLoadScene : MonoBehaviour
+//{
+//    [MenuItem("Open Scene/Load &1")]
+//    public static void OpenSceneRoot()
+//    {
+//        OpenScene("Loading");
+//    }
 
-    [MenuItem("Open Scene/Home &2")]
-    public static void OpenSceneMenu()
-    {
-        OpenScene("Home");
-    }
+//    [MenuItem("Open Scene/Home &2")]
+//    public static void OpenSceneMenu()
+//    {
+//        OpenScene("Home");
+//    }
 
-    [MenuItem("Open Scene/GamePlay &3")]
-    public static void OpenSceneGamePlay()
-    {
-        OpenScene("GamePlay");
-    }
+//    [MenuItem("Open Scene/GamePlay &3")]
+//    public static void OpenSceneGamePlay()
+//    {
+//        OpenScene("GamePlay");
+//    }
 
 
-    public static void OpenScene(string sceneName)
-    {
-        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
-        {
-            EditorSceneManager.OpenScene("Assets/Scenes/" + sceneName + ".unity");
-        }
-    }
-}
+//    public static void OpenScene(string sceneName)
+//    {
+//        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+//        {
+//            EditorSceneManager.OpenScene("Assets/Scenes/" + sceneName + ".unity");
+//        }
+//    }
+//}
